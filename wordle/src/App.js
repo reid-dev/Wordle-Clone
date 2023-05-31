f6479369e58b1ac4add77757923eda2e71641c29
Jamie Reid <jamie.robert.reid@gmail.com>
2023-05-31 08:32:51 -0400

    Basic Project Setup with empty components

---
+++ wordle/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        Wordle
+      </nav>
+        <Board />
+        <Keyboard />
+    </div>
+  );
+}
+
+export default App;
